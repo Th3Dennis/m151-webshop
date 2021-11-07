49cd29deea5653227cad6d80c986fe5ec67cd655
Dennis Miceli <dennis.miceli@hotmail.ch>
2021-11-07 22:47:55 +0100

    Changed Overview to overview

--- controller/loginController.php
+++ controller/loginController.php
@@ -19,7 +19,7 @@ if (isset($email) && isset($password)){
     if (verifyLogin($email, $password)){
         $_SESSION['email'] = $email;
         $_SESSION['loggedIn'] = true;
-        header("Location: ../Overview.php");
+        header("Location: ../overview.php");
     } else{
         $_SESSION['errorMessage'] = "E-Mail oder Passwort falsch.";
         header("Location: ../login.php");
