53aa1858be197748ffe1c335ccc71d03cee566f3
Th3Dennis <dennis.miceli@hotmail.ch>
2021-11-08 11:27:13 +0100

    Added a PayPal Link to the picture

--- payment.php
+++ payment.php
@@ -21,12 +21,15 @@ $price += $product['price'];
 
     <div class="row mt-2 mb-2">
         <div class="col"></div>
-        <div class="col"><img src="./model/photos/paypal.png" class="w-100"></div>
-        <div class="col"></div>
+        <div class="col"><a href="https://www.paypal.me/th3dennis"><img src="./model/photos/paypal.png" class="w-100"></a></div>
+        <div class="col">
+        </div>
     </div>
 
+
     <form class="row g-3 mt-2" action="./controller/deleteCart.php">
-        <div class="col"></div>
+        <div class="col">
+        </div>
         <div class="col">
             <button class="btn btn-primary w-100 float-end" type="submit">Return to HomePage</button>
         </div>
